98a88d851ff71c192e556966c69ec4ce8e95c738
Nick Amabile <amabile.n@gmail.com>
2012-04-23 07:52:16 -0700

    fix add to cart link

--- app/helpers/tickets_helper.rb
+++ app/helpers/tickets_helper.rb
@@ -1,7 +1,7 @@
 module TicketsHelper
 	def get_checkout_link(event_id,ticket_group_id,price,requested_qty)
 		p_id = 5329275
-		affiliate_url = "http://www.tkqlhce.com/click-#{p_id}-10843777?"
+		affiliate_url = "http://www.tkqlhce.com/click-#{p_id}-10843777?url="
 		broker_id = 1
 		site_number = 3
 		qty = requested_qty
@@ -9,13 +9,13 @@ module TicketsHelper
 		length = chars.length
 		i  = 0
 		session_id = ''
-		while i <= 5 do
+		while i <= 4 do
 			index = rand(length)
 			session_id += chars[index]
 			i += 1
 		end
-		tn_url = "https://secure.ticketnetwork.com/checkout.Checkout.aspx?"
-		base_url = "brokerid=#{broker_id}&sitenumber=#{site_number}&tgid=#{ticket_group_id}&evtid=#{event_id}&price=#{price}&treq=#{requested_qty}&SessionId=#{session_id}"
-		"#{affiliate_url}#{url_encode(tn_url + base_url)}"
+		tn_url = "https://secure.ticketnetwork.com/checkout/Checkout.aspx?"
+		base_url = "brokerid=#{broker_id}&sitenumber=#{site_number}&evtID=#{event_id}&tgid=#{ticket_group_id}&price=#{price}&treq=#{requested_qty}&SessionId=#{session_id}"
+		"#{affiliate_url + url_encode(tn_url + base_url)}"
 	end
 end
